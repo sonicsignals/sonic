0fb3f650cefa06df84bee44603c67b4a1bc24f34
Md. Al Imran <110711957+sonicsignals@users.noreply.github.com>
2024-09-12 16:42:27 +0600

    Add files via upload

---
+++ quotex-vew.js
@@ -0,0 +1,68 @@
+$('#formHist').submit(function(e) {
+    e.preventDefault()
+    return false
+})
+
+var _0x570564 = _0x4d63;
+!function(n, e) {
+    for (var t = _0x4d63, r = _0x2fdc(); ; )
+        try {
+            if (571090 === -parseInt(t(430)) / 1 * (parseInt(t(312)) / 2) + -parseInt(t(378)) / 3 + -parseInt(t(392)) / 4 * (-parseInt(t(345)) / 5) + parseInt(t(259)) / 6 + -parseInt(t(193)) / 7 * (-parseInt(t(597)) / 8) + -parseInt(t(586)) / 9 + parseInt(t(217)) / 10)
+                break;
+            r.push(r.shift())
+        } catch (n) {
+            r.push(r.shift())
+        }
+}();
+
+function _0x2fdc() {
+    var n = ["EDKuD", "9f05ca3053", "display", "width", "spvYn", "4|3|5", "Fim da lis", "Minimum Pe", "htKLl", "ready", "HJahV", "00% | M5 |", "cguwe", "kDif", "nextCandle", "IKNrr", "6|13|3|0|1", "wFscD", "QIoIy", "pinlG", "progress", "execComman", "error", "xOVrw", "atic.quote", "/10_en/728", "r de Sinai", "32f191f03-", "fLpzW", "WURam", "23:40", "HtOIh", "ById", "minPercent", "averageTic", "AiwZp", "djBIp", "AyqkH", "tor", "_90.png", "jGwlm", "pelo menos", "sTFIa", "RuwUL", ".titlebrok", "84f7d3107b", "JBbDx", "pFFLP", "none", "rcentage:", "stener", "/6_en/728_", "map", "UhRsB", "NlQlH", "cCjmS", "fadeOut", "otc", "hETho", "pXigT", "WCGYm", "NFXhz", "r='selDays", "KUdqj", "Lista está", "Bearer eb2", "cOBbN", "rmmwH", "Limite de ", "Maximum Pe", "CERWS", "jVzie", "50.000 vel", "pJrFa", "isar.", "overflow", "rContainer", "/7_en/728_", "bZkZY", "Loaded", "result", "2|1|2|11|4", "ZFehd", "forEach", "jlwYx", "HbNFA", "lpFpN", "SpUPh", "oNdzz", "WyjWA", "time", "as excedid", ".checkbox-", "UGIlg", "AtzOd", "JePcX", "Todas as C", "RJBDq", "wqvNJ", "| 00:30 > ", "ontainer", "days", "YMjHc", "r='endTime", "pLryw", "erdUe", "Marco de t", "src", "srAVj", "jmjFb", "r='startTi", "language", "HRWkq", "para pesqu", "fABuJ", "Asset:", "DOMContent", "16:30", "https://ap", "mpsoX", "PBsqJ", "LLMmS", "slow", "JFKnF", "⭐️  90% | 1", "/9_id/728_", "rJPED", "Martingale", "DSwnC", "326208921b", "Timeframe:", "innerHTML", "_90.jpg", "PpgIa", "nzdWf", "vByht", "Liebh", "instrument", "label", "UvRwc", "lsQth", "JrCOy", "3291795jiJkSb", "ajax", "jGCXO", "Direction:", "|8|10|9|7|", "myText", "nyoGD", "lista:", "KieEq", "/10_id/728", "value", "6479144GnNBVd", "gbqjv", "TWfOT", "ZTSDy", "sByClassNa", ".configg", "413a877288", "0|2|4|1|3", "&count=", "qUVzc", "Sfgml", "BWXzW", "FTuzj", "yldDT", "#cbAtivo", "CcQgy", "sIpxL", "Carregando", "hXdyq", "mdHnZ", "/10_pt/728", "All Broker", "/15_pt/728", "vwJQB", "padStart", ".form-grou", "KzATZ", "entageMax'", "split", "myModal", "x.io/files", "endTime", "hdKsE", ".progress", "EQShT", "gMIkm", "GkoJE", "eTadb", "pt-br", "mAYGO", "Inicio de ", "IrlRS", "EIZhQ", "KptYk", "pXekv", "Hcwwn", "candles", "Jvbbg", "mendt", "chooseAsse", "querySelec", "DqQZd", "pVRsq", "Martingala", "KlOll", "AMDIb", "maxPercent", "r de Señal", "SStkq", "0|1|7|4|6|", "APceX", "JPUvG", "TgNJR", "buttonX", "container ", "gales", "Todas las ", "IrkXt", "#selMartin", "BrhJq", "VyfNl", "#selCandle", "me']", "ewwtZ", "0|2|6|7|1|", "CCJOx", "...", "title", "CVZXM", "HQbde", "#selOrderT", "Juiel", "ta:", "ce.oanda.c", "otcCheckbo", "lay", "GERAR LIST", "16:00", "7XrDYUl", "copyNotifi", "amYXT", "body", "zMQJa", "UvAya", "sort", "VEzmM", "OTC?", "UIWUl", "Gales:", "textConten", "WvdOl", "JJvst", "disabled", "TvSgf", "BVOgZ", "FlMWQ", "win", "copy", "WtPQP", "martingale", "/18_id/728", "fdWQC", "9112780fNsjGv", "aloger", "ligCE", "DRAW", "Time", "p label[fo", "VxLha", "upSpN", "fSvVX", "Dias:", "age", "EnrUX", "filter", "Days:", "Selecione ", "IvEiD", ".subtitlet", "esUeX", "winrate", "pair", "HNJEQ", "GnsmB", "default", "LlKjj", "m mínima:", "/17_id/728", "lJuhn", "fxAlj", "3|2|8|5", "/8_pt/728_", ".preload-c", "cyBAp", "Porcentaje", "#selTimeEn", "NbrXF", "QFAjK", "Fin de la ", "r='selPerc", "GET", "entageMin'", "percentDif", "uyRBN", "672798pmpQPa", "addEventLi", "closes", "SEvAH", "DDTCK", "Porcentage", "body, html", " list:", "Corredoras", "ype", "r='ativos'", "tageMax", "qkLtq", "momMR", "HGmAr", "BlUYl", "GPgTJ", "90.jpg", "SXfFY", "TZmsi", "EcpPt", "onload", "01:00", "torAll", "cReGF", "fPgFM", "nzMOt", "Direção:", "BCXSg", "startTime", "XzumM", "show", " vazia.", "Inicio da ", "ipCil", "from", "tlvwH", "WXgJu", ".content", "block", "GoeoS", "AGUARDE", "progressBa", "/candles?g", "getElement", "wcarb", "ranularity", "kxkam", "orFYW", "css", "winEJ", "itlebroker", "d9be68b748", "20FRvXYR", "Dirección:", "ulCph", "Catalogado", "EKFKS", "/16_pt/728", "RrsqT", "twpaa", "timeframe", "length", "oXRmN", "nKfwZ", "Start of t", "FVsVO", "style", "cfzFJ", "replace", "yetXD", "iPFNH", "iXQLm", "oTFyK", "ZdrbN", "GRhpp", "checked", "mid", " 18D | 0G ", "mVyWf", "dyRbO", "ZPLtV", "kZlNg", "SjVHt", "la lista:", "#selTimeIn", "3465XuLGaK", "#selPercen", "he list:", "EiMMG", "RirUN", "CALL", "m máxima:", "LpDgR", "PUT", "PCnFd", "DuGQc", "Ativo:", "DZXqc", "-OTC", "eTjzY", "ZvLGN", "select", "lhOyO", "wILfX", "onclick", "cation", " um ativo ", "auto", "val", "dkGAe", "dsLxu", "date", "Días:", "GrLbM", "hide", "JjDYg", "resultValu", "nAeNW", "3277038DwzjAy", "HmItq", "getMinutes", "/14_pt/728", "ZhhPT", "orretoras", "qgAZm", "gcCuG", "ruments/", "tageMin", "Signal Cat", "direction", "subtitle", "om/v3/inst", "5596pGWonS", "xravJ", "MnrTC", "tcqIc", "HvRHE", "i-fxpracti", "kQRaI", "ZNTTK", "CsWsT", " mínimo:", "getHours", "YFxYT", "JbSTB", "FGAmw", "/19_id/728", "/11_pt/728", "GgkUL", "ZfJGT", "toString", "zljqM", "Pmerj", "IhvLs", "image-disp", "fjapw", "nhqGo", "Rlkrz", "cursor", "iempo:", "End of the", "vFgcy", "ZzIHv", " máximo:", "TQjgd", "Jbkpi", "GQtTk", "fRsxX", "SkLiC", "https://st", "77362qRtNzr", "Activo:", "target", "qidbS", "HDOpf", "ZJqem", "config", "QOJlh", "gdsrf", "18:00", "find", "push", "hVqnT", "#selDays"];
+    return (_0x2fdc = function() {
+        return n
+    }
+    )()
+}
+
+
+
+function _0x4d63(n, e) {
+    var t = _0x2fdc();
+    return (_0x4d63 = function(n, e) {
+        return t[n -= 113]
+    }
+    )(n, e)
+}
+
+
+function sleep(n) {
+    return new Promise((e=>setTimeout(e, n)))
+}
+var images = [_0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(317) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(127) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(381) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(407) + _0x570564(576), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(125) + _0x570564(576), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(246) + _0x570564(276), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(469) + _0x570564(576), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(521) + _0x570564(276), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(495) + _0x570564(276), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(406) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(215) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(242) + _0x570564(483), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(595) + _0x570564(576), _0x570564(429) + _0x570564(468) + _0x570564(135) + _0x570564(569) + _0x570564(276)]
+  , currentIndex = 0
+  , imageDisplay = document[_0x570564(303) + _0x570564(476)](_0x570564(414) + _0x570564(190));
+function changeImage() {
+    var n = _0x570564
+      , e = {
+        mendt: function(n, e) {
+            return n % e
+        },
+        SkLiC: function(n, e) {
+            return n + e
+        }
+    };
+    imageDisplay[n(551)] = images[currentIndex],
+    currentIndex = e[n(153)](e[n(428)](currentIndex, 1), images[n(321)])
+}
+changeImage(),
+setInterval(changeImage, 3e3),
+document[_0x570564(260) + _0x570564(494)](_0x570564(560) + _0x570564(523), (function() {
+    var n = _0x570564
+      , e = {
+        mAYGO: function(n, e) {
+            return n(e)
+        },
+        cOBbN: n(143)
+    };
+    e[n(144)](translateForm, e[n(510)])
+}
+))
